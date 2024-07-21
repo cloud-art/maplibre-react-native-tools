350eab5d3a1f12f9949f5cdaac89d839a93fb6fe
art-clouds <artyushenko.k.r1@gmail.com>
2024-07-21 19:59:52 +0300

    chore: preparing to publish

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export * from './components';
+export * from './constants';
+export * from './context';
+export * from './hooks';
+export * from './types';
+export * from './utils';
+import mapStyleJson from './mapStyle.json';
+export const mapStyle = mapStyleJson;
