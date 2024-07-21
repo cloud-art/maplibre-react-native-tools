c769d9c4fc9c507930b8bf3d557249bde5ba2a26
art-clouds <artyushenko.k.r1@gmail.com>
2024-07-21 10:37:24 +0300

    refactor(eslint.config.mjs): eslint config changed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,114 @@
-import { fixupConfigRules } from '@eslint/compat';
-import pluginJs from '@eslint/js';
-import prettierConfig from 'eslint-config-prettier';
-import pluginPrettier from 'eslint-plugin-prettier';
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+// @ts-check
 
-export default tseslint.config(
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  ...fixupConfigRules(pluginReactConfig),
-  ...fixupConfigRules(prettierConfig),
-  { ignores: ['node_modules', 'dist'] },
+import react from '@eslint-react/eslint-plugin';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import reactJsxRuntime from 'eslint-plugin-react/configs/jsx-runtime.js';
+import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
+import globals from 'globals';
+import tsEslint from 'typescript-eslint';
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+export default tsEslint.config(
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommendedTypeChecked,
   {
+    files: ['**/*.{js,ts,jsx,tsx,mjs,mts,cjs}'],
+    ignores: ['node_modules/*'],
+  },
+  {
+    files: ['**/*.{js,jsx,mjs,cjs}'],
+    extends: [tsEslint.configs.disableTypeChecked],
+  },
+  {
+    files: ['eslint.config.mjs'],
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+      globals: globals.node,
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    ...react.configs['recommended-type-checked'],
+  },
+  reactRecommended,
+  reactJsxRuntime,
+  eslintConfigPrettier,
+  {
+    rules: {
+      'react/boolean-prop-naming': [
+        'error',
+        {
+          validateNested: true,
+        },
+      ],
+      'react/destructuring-assignment': [
+        'error',
+        'always',
+        { destructureInSignature: 'always' },
+      ],
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+      'react/jsx-sort-props': [
+        'error',
+        {
+          callbacksLast: true,
+          shorthandFirst: true,
+          multiline: 'last',
+          ignoreCase: true,
+          reservedFirst: true,
+        },
+      ],
+      'react/hook-use-state': ['error', { allowDestructuredState: true }],
+      'react/jsx-boolean-value': ['error', 'never'],
+      'react/jsx-key': ['error', { checkFragmentShorthand: true }],
+      '@typescript-eslint/no-import-type-side-effects': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: true,
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'separate-type-imports',
+        },
+      ],
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        {
+          checksVoidReturn: false,
+        },
+      ],
+      'boundaries/element-types': 'off',
+    },
+    languageOptions: {
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
-        globals: globals.browser,
+        globals: globals.node,
         project: ['./tsconfig.json'],
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
-    files: ['**/*.{js,jsx,mjs,cjs}'],
-    extends: tseslint.configs.disableTypeChecked,
-  },
-  {
-    plugins: { prettier: pluginPrettier },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
   },
 );
