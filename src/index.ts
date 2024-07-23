99ebf6e2ed6436b5f0658c2e1e7d98d7bd13f65f
artyushenkokr <artyushenkokr@datum-soft.ru>
2024-07-23 17:50:00 +0300

    chore

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import mapStyleJson from './mapStyle.json';
+export const mapStyle = mapStyleJson;
 export * from './components';
 export * from './constants';
 export * from './context';
 export * from './hooks';
 export * from './types';
 export * from './utils';
-import mapStyleJson from './mapStyle.json';
-export const mapStyle = mapStyleJson;
+
