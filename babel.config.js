21e25807dc852671ea2be59c16a0fff538bc5c4a
art-clouds <artyushenko.k.r1@gmail.com>
2024-07-24 18:17:03 +0300

    feat(babel.config.js): module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': './src',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
